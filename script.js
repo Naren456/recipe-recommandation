423b80ad63cacf386af521b01a42735b52cb5648
Naren456 <narendrase666@gmail.com>
2025-01-12 18:59:49 +0530

    first final

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let btn = document.querySelector(".submit")
 let textArea = document.querySelector(".Text-Area")
+let mainContainer = document.querySelector(".main-container")
 btn.addEventListener('click',function(){
   let  value = textArea.value;
   console.log(value)
